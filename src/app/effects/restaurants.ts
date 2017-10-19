8ec0605aa6063983b70da43a72ae4648538ca6a5
Ben Glynn <ben.glynn@just-eat.com>
2017-10-19 17:22:49 +0100

    Effect core from observable registry #12

--- src/app/effects/restaurants.ts
+++ src/app/effects/restaurants.ts
@@ -2,20 +2,32 @@ import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store';
 import { Actions, Effect } from '@ngrx/effects';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/mergemap';
+import 'rxjs/add/operator/zip';
 import * as fromRestaurants from '../reducers/restaurants';
 import { RestaurantService } from '../services/restaurant.service';
 
 @Injectable()
 export class RestaurantEffects {
+
+  effectCore$ = Observable.from([
+    function loadRestauants(action: fromRestaurants.LoadRestaurants): Observable<Action> {
+      return this.restaurantService.getRestaurants(action.payload)
+        .map(data => new fromRestaurants.UpdateRestaurants(data));
+    }]);
+
   @Effect() loadRestauants$: Observable<Action> = this.actions$
     .ofType<fromRestaurants.LoadRestaurants>(fromRestaurants.LOAD_RESTAURANTS)
-    .mergeMap(action => this.restaurantService.getRestaurants(action.payload)
-      .map(data => new fromRestaurants.UpdateRestaurants(data)));
+      .zip(this.effectCore$.filter(fn => fn.name === 'loadRestauants'))
+      .map(values => {
+        const [action, fn] = values;
+        return fn.call(this, action);
+      })
+      .mergeMap(input => input);
 
   constructor(
     private actions$: Actions,
     private restaurantService: RestaurantService
   ) {}
-
 }
