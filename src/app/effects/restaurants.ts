168399473fcf91e876ffa732a14a549b3e34b727
Ben Glynn <ben.glynn@just-eat.com>
2017-10-20 11:37:19 +0100

    Refactored to always pull effect core on action

--- src/app/effects/restaurants.ts
+++ src/app/effects/restaurants.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { Actions, Effect } from '@ngrx/effects';
 import { EffectCores } from '.';
 import 'rxjs/add/operator/mergemap';
+import 'rxjs/add/operator/combinelatest';
 import 'rxjs/add/operator/zip';
 import * as fromRestaurants from '../reducers/restaurants';
 import { RestaurantService } from '../services/restaurant.service';
@@ -13,12 +14,13 @@ export class RestaurantEffects {
 
   @Effect() loadRestauants$: Observable<Action> = this.actions$
     .ofType<fromRestaurants.LoadRestaurants>(fromRestaurants.LOAD_RESTAURANTS)
-    .zip(this.effectCores.store$.filter(
-      effectCore => effectCore.name === 'loadRestaurants'))
-    .map(([action, effectCore]) => effectCore.fn.call(this, action))
-    .mergeMap(input => input);
+    .combineLatest(this.effectCores.store$
+      .filter(effectCore => effectCore.name === 'loadRestaurants'),
+      (action, effectCore) => effectCore.fn.call(this, action)
+    )
+    .mergeMap(result => result);
 
-  loadRestauantsCore(action: fromRestaurants.LoadRestaurants) {
+  loadRestauantsCore$(action: fromRestaurants.LoadRestaurants) {
     return this.restaurantService.getRestaurants(action.payload)
       .map(data => new fromRestaurants.UpdateRestaurants(data));
   }
@@ -28,6 +30,6 @@ export class RestaurantEffects {
     private restaurantService: RestaurantService,
     private effectCores: EffectCores
   ) {
-    this.effectCores.register('loadRestaurants', this.loadRestauantsCore);
+    this.effectCores.register('loadRestaurants', this.loadRestauantsCore$);
   }
 }
