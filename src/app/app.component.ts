426707a59de0bb62fea056a68b157ace079d82aa
Ben Glynn <ben.glynn@just-eat.com>
2017-11-14 13:30:00 +0000

    Separate console areas #31

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from './store/store';
+import { Observable } from 'rxjs/Observable';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +10,24 @@ import { Store } from './store/store';
 })
 export class AppComponent {
 
-  constructor( public store: Store ) {}
+  filteredState$: Observable<any>;
+  config$: Observable<any>;
+  trace = '';
+  adaptedView = false;
 
-  onTapAdapt() {
+  constructor( public store: Store ) {
+    this.filteredState$ = this.store.state$
+      .map(state => Object.entries(state)
+      .reduce((prev, [name, value], index) =>
+        (name === 'configuration') ? prev : Object.assign(prev, {[name]: value}),
+        {})
+    );
+    this.config$ = this.store.select('configuration');
+    this.store.action$.subscribe(action => this.trace += action.type + '\n');
+  }
+
+  onTapAdaptView() {
     this.store.dispatch({type: 'INIT_ADAPT_RESULT_VIEW'});
+    this.adaptedView = true;
   }
 }
