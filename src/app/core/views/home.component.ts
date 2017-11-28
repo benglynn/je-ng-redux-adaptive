d2f6ad90c979c3d557e0dbf1061112c7b9ce33a9
Ben Glynn <ben.glynn@just-eat.com>
2017-11-28 10:23:35 +0000

    Scoped style; common breakpoints #54

--- src/app/core/views/home.component.ts
+++ src/app/core/views/home.component.ts
@@ -6,6 +6,7 @@ import { IAreaState } from '../../area/state';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
