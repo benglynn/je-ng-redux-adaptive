b90b28758d5ec653b330d444835ce2be1db75436
Ben Glynn <ben.glynn@just-eat.com>
2018-02-01 13:52:42 +0000

    Service injects new restaurant #69

--- src/app/adapt-service/new-restaurants.service.ts
+++ src/app/adapt-service/new-restaurants.service.ts
@@ -3,12 +3,37 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
 import 'rxjs/add/operator/delay';
 import { IRestaurant } from '../restaurant';
+import { RestaurantsService } from '../restaurants';
 
 @Injectable()
 export class NewRestaurantsService {
 
+  restaurantsServie: RestaurantsService;
+
+  constructor(restaurantsService: RestaurantsService) {
+    this.restaurantsServie = restaurantsService;
+  }
+
   getRestaurants(area: string): Observable<IRestaurant[]> {
-    return Observable.from([])
-    .delay(500);
+    return this.restaurantsServie.getRestaurants(area)
+      .map(restaurants => {
+        const newRestaurants = restaurants;
+        newRestaurants.unshift(<IRestaurant>{
+        cuisines: 'Curry',
+        deliveryCost: 2,
+        deliveryStartTime: '',
+        drivingDistance: 0.1,
+        isCollectNow: true,
+        isDeliveryNow: true,
+        isOpen: true,
+        isSponsored: true,
+        openingTime: '',
+        percentOff: 50,
+        rating: 6,
+        ratings: 150,
+        title: 'Your favourite place!',
+        })
+        return restaurants;
+      });
   }
 }
